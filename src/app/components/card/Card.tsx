c465517593de02cb3be7ea90d8eee5eb7ea2ac2d
darkhorse0725 <darktop0112@gmail.com>
2025-07-16 09:37:24 -0400

    update

--- src/app/components/card/Card.tsx
+++ src/app/components/card/Card.tsx
@@ -8,7 +8,7 @@ const Card = ({ userInfo, index }: { userInfo: Userinfo, index: number }) => {
     return (
         <Link href={`/bot/?no=${index}`}>
             <div
-                className={`bg-cover bg-center rounded-4xl  w-[190px] h-[260px] sm:w-[300px] md:w-[calc((100% - 160px - 80px) / 5)] sm:h-[400px] grayscale-50 relative cursor-pointer`}
+                className={`bg-cover bg-center rounded-xl md:rounded-4xl  w-[170px] h-[260px] sm:w-[300px] md:w-[calc((100% - 160px - 80px) / 5)] sm:h-[400px] grayscale-50 relative cursor-pointer`}
                 style={{ backgroundImage: `url('assets/img/pitgirls/${url}')` }}
             >
                 <div className="absolute bottom-0 w-full align-middle text-center p-3">
@@ -17,17 +17,17 @@ const Card = ({ userInfo, index }: { userInfo: Userinfo, index: number }) => {
                             <img className="rounded-xl" src={`https://www.countryflags.com/wp-content/uploads/${nationality.toLowerCase()}-flag-png-large.png`} alt="flg" />
                         </div>
                         <div className="flex flex-col pl-1">
-                            <div className="flex text-white font-bold text-[12px] sm:text-[18px]">
+                            <div className="flex text-white font-bold text-[10px] sm:text-[18px]">
                                 <div className="">Name:</div>
                                 <div className="">{name}</div>
                             </div>
-                            <div className="flex text-white font-bold text-[12px] sm:text-[16px]">
+                            <div className="flex text-white font-bold text-[9px] sm:text-[16px]">
                                 <div className="">{age} YRS - {nationality.toUpperCase()}</div>
                             </div>
                         </div>
                     </div>
                     <div className="flex pt-2">
-                        <div className="text-left text-white text-[12px] sm:text-[16px]">{description.substring(0, 100)} ...</div>
+                        <div className="text-left text-white text-[8px] sm:text-[16px]">{description.substring(0, 100)} ...</div>
                     </div>
                 </div>
 
